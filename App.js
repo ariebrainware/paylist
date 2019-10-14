d9e7bb7ee134d653d84e13ace288a7825d5b2811
Arie Brainware <publicu001@protonmail.com>
2019-10-14 10:01:20 +0700

    Fix linting

--- App.js
+++ App.js
@@ -31,8 +31,7 @@ export default function App(props) {
 async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
+      require('./assets/images/add-button.png'),
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
