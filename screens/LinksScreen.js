f8756721cdde62dbbb2e8afef7ac53ff25a34d08
Arie Brainware <publicu001@protonmail.com>
2019-10-28 16:18:13 +0700

    Update about page

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,40 +1,49 @@
 import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View, Text, Linking } from 'react-native'
 import { Card, Title, Paragraph } from 'react-native-paper'
 
 export default function LinksScreen() {
   return (
     <View style={styles.container}>
-    <ScrollView >
-      <Card>
-        <Card.Content style={{alignItems:'center', paddingTop:0}}>
-          <Title>PAYLIST APP</Title>
-        </Card.Content>
-        <Card.Content>
-          <Paragraph>Pay bill, set budget per month, and jot down some buy list easily with Paylist</Paragraph>
-        </Card.Content>
-      </Card>
-    </ScrollView>
-    <View>
-    <Card>
-      <Card.Content style ={{alignContent:'space-around', position:'relative', left:0, right:0 }}>
-        <Title>
-          paylist teams :
-        </Title>
-        <Paragraph>
-          Arie Brainware, Offler
-        </Paragraph>
-      </Card.Content>
-    </Card>
-  </View>
-  </View>
+      <ScrollView >
+        <Card>
+          <Card.Content style={{ alignItems: 'center', paddingTop: 0 }}>
+            <Title>PAYLIST APP</Title>
+          </Card.Content>
+          <Card.Content>
+            <Paragraph>Pay bill, set budget per month, and jot down some buy list easily with Paylist</Paragraph>
+          </Card.Content>
+        </Card>
+      </ScrollView>
+      <View>
+        <Card>
+          <Card.Content style={{ alignContent: 'space-around', position: 'relative', left: 0, right: 0 }}>
+            <Title>
+              Paylist Team :
+            </Title>
+            <Paragraph>
+              <Text style={{ color: 'blue' }}
+                onPress={() => Linking.openURL('https://twitter.com/_nobody404')}>
+                Arie Brainware
+              </Text>
+            </Paragraph>
+            <Paragraph>
+              <Text style={{ color: 'blue' }}
+                onPress={() => Linking.openURL('https://twitter.com/offler_7')}>
+                Offler Dapit
+              </Text>
+            </Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+    </View>
   )
 }
 
 LinksScreen.navigationOptions = {
   title: 'About',
-  headerStyle:{
-    backgroundColor:'#a9b0ae'
+  headerStyle: {
+    backgroundColor: '#a9b0ae'
   }
 }
 
@@ -44,10 +53,10 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
-  Content:{
-    fontWeight:'bold',
+  Content: {
+    fontWeight: 'bold',
     fontSize: 12,
-    fontStyle:"italic",
-    fontFamily:"sans-serif"
+    fontStyle: "italic",
+    fontFamily: "sans-serif"
   }
 })
